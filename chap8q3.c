a75887a496662018fc0fcf53a535aaa763902b8c
Saalim-31 <134718581+Saalim-31@users.noreply.github.com>
2023-08-22 16:03:29 +0100

    Update chap8q3.c

--- chap8q3.c
+++ chap8q3.c
@@ -1,7 +1,7 @@
 //program to calculate one number raised to another number using loops and functions
 
 #include <stdio.h>
-int power(int  ,int );
+int power(int  ,int );             function decleration
 int main() {
     int base, exp ,res;
 
@@ -9,18 +9,18 @@ int main() {
     scanf("%d", &base);
     printf("Enter an exponent: ");
     scanf("%d", &exp);
-        res=power(base,exp);
+        res=power(base,exp);             //function call
     printf("%d raised to the power %d = %d",base,exp,res);
     return 0;
 }
 
 
-        int power(int base ,int exp)
+        int power(int base ,int exp)             //functions definition
 {
 
 
             int result=1,i;
-            for (i=1;i<=exp;i++)
+            for (i=1;i<=exp;i++)                    //logic using loops 
                 result=result*base;
             return result;
 }
